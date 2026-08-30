2c136985b8024b5d32f9782db5e9ba133544a994
pedro1287 <102392245+pedro1287@users.noreply.github.com>
2026-08-29 22:59:17 -0400

    Update mediafire.py

--- pydownloader/mediafire.py
+++ pydownloader/mediafire.py
@@ -4,13 +4,13 @@ import requests
 import user_agent
 
 def get(url):
-    if re.match("download[0-9]*\.mediafire\.com", url.lstrip("https://").lstrip("http://").split("/")[0]):
+    if re.match(r"download[0-9]*\.mediafire\.com", url.lstrip("https://").lstrip("http://").split("/")[0]):
         data = url.lstrip("https://").lstrip("http://").split("/")
         if len(data) <= 2:
             raise Exception("Invalid mediafire download url")
         unique_id = data[2]
 
-    elif re.match("[w]*\.mediafire\.com", url.lstrip("https://").lstrip("http://").split("/")[0]):
+    elif re.match(r"[w]*\.mediafire\.com", url.lstrip("https://").lstrip("http://").split("/")[0]):
         data = url.lstrip("https://").lstrip("http://").split("/")
         if len(data) <= 2:
             raise Exception("Invalid mediafire download url")
